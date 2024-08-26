9d4f088523b41916eac669087d8e4a43c664cfc2
Meleneth <meleneth@gmail.com>
2024-08-26 21:12:36 +0000

    Add tests for key event multi handling

--- src/cnegng/ACME/pygame_event_handler.py
+++ src/cnegng/ACME/pygame_event_handler.py
@@ -1,15 +1,33 @@
 import pygame
 
+
 class DuplicateHandlersError(Exception):
     pass
 
+
+class KeyEventHandler:
+    def __init__(self):
+        self.key_event_handlers = {}
+
+    def register_key_event(self, key, handler_method):
+        if key in self.key_event_handlers:
+            raise DuplicateHandlersError(
+                f'Duplicate key handler configured for key "{key}"'
+            )
+        self.key_event_handlers[key] = handler_method
+
+    def __call__(self, event):
+        if (key_event := self.key_event_handlers.get(event.key)) is not None:
+            key_event(event)
+
+
 class PyGameEventHandler:
     """ACME module for data-driven event  handler dispatch"""
+
     def __init__(self):
-        """
-        """
+        """ """
         self.event_handlers = {}
-    
+
     def handle_events(self, event_list=pygame.event):
         """Consume all pygame events and call handle_event(event) on each one
 
@@ -23,8 +41,8 @@ class PyGameEventHandler:
 
         :param event: usually the result of pygame.event.get()
         """
-        if event.type in self.event_handlers:
-            self.event_handlers[event.type](event)
+        if (event_handler := self.event_handlers.get(event.type)) is not None:
+            event_handler(event)
 
     def register_event_handler(self, event_type, handler_method):
         """Register a callback for when a specific event type comes in.
@@ -34,5 +52,38 @@ class PyGameEventHandler:
 
         """
         if event_type in self.event_handlers:
-            raise DuplicateHandlersError(f"Tried to give a second handler for event_type({event_type})")
+            raise DuplicateHandlersError(
+                f"Tried to give a second handler for event_type({event_type})"
+            )
         self.event_handlers[event_type] = handler_method
+
+    def register_keydown_event_handler(self, key, handler_method):
+        """Register a callback for a specific key down event handler is called
+
+        :param key: the key to react to
+        :param handler_method: the method to call when this event is handled.  It works well to use a method from a class that inherits from PyGameEventHandler
+
+        """
+        self.register_key_event_handler(pygame.KEYDOWN, key, handler_method)
+
+    def register_keyup_event_handler(self, key, handler_method):
+        """Register a callback for a specific key up event handler is called
+
+        :param key: the key to react to
+        :param handler_method: the method to call when this event is handled.  It works well to use a method from a class that inherits from PyGameEventHandler
+
+        """
+        self.register_key_event_handler(pygame.KEYUP, key, handler_method)
+
+    def register_key_event_handler(self, event_type, key, handler_method):
+        """Register a callback for a specific key event handler is called
+
+        :param event_type: the event type we are registering a handler for.
+        :param key: the key to react to.  If it already exists, DuplicateHandlersError will be raised instead
+        :param handler_method: the method to call when this event is handled.  It works well to use a method from a class that inherits from PyGameEventHandler
+
+        """
+        if event_type not in self.event_handlers:
+            self.event_handlers[event_type] = KeyEventHandler()
+        # if it blows up here because NoMethodError for register_key_event, it means something is already configured to listen to this event_type that isn't using a KeyEventHandler
+        self.event_handlers[event_type].register_key_event(key, handler_method)
--- tests/ACME/test_pygame_event_handler.py
+++ tests/ACME/test_pygame_event_handler.py
@@ -1,6 +1,8 @@
 from flexmock import flexmock
+import pygame
 
 from cnegng.ACME import PyGameEventHandler
+
 
 class SpecificEventHandler(PyGameEventHandler):
     def __init__(self):
@@ -11,9 +13,29 @@ class SpecificEventHandler(PyGameEventHandler):
     def handle_specific_event(self, event):
         self.some_flag = True
 
+class MultiKeyEventHandler(PyGameEventHandler):
+    def __init__(self):
+        self.some_value = 0
+        super().__init__()
+        self.register_keydown_event_handler(pygame.K_UP, self.handle_specific_event)
+        self.register_keydown_event_handler(pygame.K_w, self.handle_specific_event)
+
+    def handle_specific_event(self, event):
+        self.some_value = self.some_value + 1
+
 def test_pygame_event_handler():
     event_handler = SpecificEventHandler()
-    flag = False
     fake_event = flexmock(type="some_event_type")
+    assert not event_handler.some_flag
     event_handler.handle_event(fake_event)
     assert event_handler.some_flag
+
+def test_pygame_key_event_handler():
+    event_handler = MultiKeyEventHandler()
+    assert event_handler.some_value == 0
+    fake_event = flexmock(type=pygame.KEYDOWN, key=pygame.K_UP)
+    event_handler.handle_event(fake_event)
+    assert event_handler.some_value == 1
+    fake_event = flexmock(type=pygame.KEYDOWN, key=pygame.K_w)
+    event_handler.handle_event(fake_event)
+    assert event_handler.some_value == 2
