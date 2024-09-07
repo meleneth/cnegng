56236e581a78b9de69aa9264206c49f5671cd801
Meleneth <meleneth@gmail.com>
2024-09-07 07:09:28 +0000

    close to dockerized docs deploy to my internal cluster

---
+++ build_documentation.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -euo pipefail
+
+docker buildx build -f Dockerfile.sphinxdocs -t registry.sectorfour/meleneth/cnegng-docs:latest --push .
+kubectl get pods | grep cnegng_docs | awk '{print $1}' | xargs kubectl delete pod
